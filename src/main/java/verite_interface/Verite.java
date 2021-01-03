c3c64be82d78d1c216e6a378efe5d779c0206964
awadiaby <awa.diaby@etudiant.univ-rennes1.fr>
2021-01-03 18:17:24 +0100

    add

--- src/main/java/verite_interface/Verite.java
+++ src/main/java/verite_interface/Verite.java
@@ -3,31 +3,47 @@ package verite_interface;
 import extracteur_java.wikiMainhtml;
 import extracteur_java.wikiMainwikitext;
 import javafx.application.Application;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.embed.swing.SwingNode;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.control.cell.TextFieldListCell;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import javafx.util.Callback;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import javax.swing.*;
+import javax.swing.table.JTableHeader;
+import java.awt.*;
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.concurrent.atomic.AtomicReference;
 
 
 public class Verite extends Application {
-    TableView table = new TableView();
-    private final ObservableList<Object> dataList
-            = FXCollections.observableArrayList();
+    private TableView<Object> table = new TableView();
+    private final ObservableList<Object> dataList = FXCollections.observableArrayList();
     int numeroTableau = 0;
+    Scene scene;
+    Stage stage;
+    JTable table_;
+    JPanel conteneurTableau;
+    ScrollPane scrollPane;
+    String[][] contentTable;
+    String[] columnNames;
+    SwingNode swingNode;
+    String savedFileName = "";
+    String typeExtractor = "";
+
+    int nombreMaxTableau = 0;
+    int oldNumber = 0;
+
+
 
 
     public static void main(String[] args) {
@@ -42,48 +58,70 @@ public class Verite extends Application {
         comboBox.getItems().add("Extracteur Python");
         comboBox.getItems().add("Extracteur Java Html");
         comboBox.getItems().add("Extracteur Java Wikitext");
-        Label label2 = new Label("Entrer un numéro");
-        TextField textField1 = new TextField();
-        Button button1 = new Button("Valider");
+        TextField textNumeroTableau = new TextField();
+        Button valider = new Button("Valider");
         HBox urlBox = new HBox();
         urlBox.getChildren().addAll(label1, textField);
         HBox hBox = new HBox(comboBox);
-        HBox hBox1 = new HBox(textField1);
-        HBox hBox2 = new HBox(button1);
+        HBox hBox1 = new HBox(textNumeroTableau);
+        HBox boxBoutonValid = new HBox(valider);
 
-        textField1.textProperty().addListener((observable, oldValue, newValue) -> {
-            if (newValue.equals(""))
+
+        textNumeroTableau.textProperty().addListener((observable, oldValue, newValue) -> {
+
+            if (newValue.equals("")){
                 numeroTableau = 0;
-            else
-                numeroTableau = Integer.parseInt(newValue);
+            }
+            else {
+                try {
+                    numeroTableau = Integer.parseInt(newValue);
+                } catch(Exception e) {
+                    numeroTableau = 0;
+                }
+            }
+            savedFileName = savedFileName.replace(oldNumber+"",numeroTableau+"");
+
+
+            ArrayList<String[]> csvLines = readCSV(typeExtractor, savedFileName);
+            columnNames = csvLines.get(0);
+            contentTable = new String[csvLines.size()-1][];
+            for (int i = 1; i < csvLines.size(); i++) {
+                contentTable[i-1] = csvLines.get(i);
+            }
+
+            table_ = new JTable(contentTable,columnNames);
+            conteneurTableau.paint(null);
+            conteneurTableau.add(new JScrollPane(table_), BorderLayout.CENTER);
+            createSwingContent(swingNode);
+
+            System.out.println(numeroTableau+" : ("+columnNames.length+","+contentTable.length+")");
+
+            oldNumber = numeroTableau;
+
+
         });
 
-//        button1.setOnAction(e ->
-//                {
-//
-//                    label2.setText("The name you entered is " + button1.getText());
-//                }
-//        );
+
         AtomicReference<String> url = new AtomicReference<>(textField.getText());
         comboBox.setOnAction(e ->
                 {
                     switch (comboBox.getValue()) {
                         case "Extracteur Python":
+                            typeExtractor = " ";
                             url.set(textField.getText());
-                            //showCSVTable(url.get());
-                            //todo
                             break;
                         case "Extracteur Java Html":
+                            typeExtractor = "html";
                             url.set(textField.getText());
                             //verifier que contents contient un fichier qui commence par l'url
                             File directoryPath = new File("src/main/output/html");
                             //List of all files and directories
                             String[] contents = directoryPath.list();
 
-                            if (textField1.getText().equals(""))
+                            if (textNumeroTableau.getText().equals(""))
                                 numeroTableau = 0;
                             else
-                                numeroTableau = Integer.parseInt(textField1.getText());
+                                numeroTableau = Integer.parseInt(textNumeroTableau.getText());
                             boolean fileInFolder = false;
                             if (contents != null) {
                                 for (String filename : contents) {
@@ -108,16 +146,16 @@ public class Verite extends Application {
 
                             break;
                         case "Extracteur Java Wikitext":
+                            typeExtractor = "wikitext";
                             url.set(textField.getText());
-                            //verifier que contents contient un fichier qui commence par l'url
                             File directoryPath1 = new File("src/main/output/html");
                             //List of all files and directories
                             String[] contents1 = directoryPath1.list();
 
-                            if (textField1.getText().equals(""))
+                            if (textNumeroTableau.getText().equals(""))
                                 numeroTableau = 0;
                             else
-                                numeroTableau = Integer.parseInt(textField1.getText());
+                                numeroTableau = Integer.parseInt(textNumeroTableau.getText());
                             boolean fileInFolder1 = false;
                             if (contents1 != null) {
                                 for (String filename : contents1) {
@@ -147,55 +185,101 @@ public class Verite extends Application {
 
 
         //avant d'appeler il faut appeler les extracteurs d'abord
+        valider.setOnAction(e->{
+//            System.out.println(contentTable);
+            for(String[] ligne:contentTable){
+                for(String cell:ligne)
+                    System.out.print(cell +" ");
+                System.out.println();}
 
+            save(savedFileName);
+        });
+
+        swingNode = new SwingNode();
         VBox root = new VBox();
-        root.getChildren().addAll(urlBox, hBox, table, hBox1, hBox2);
+        root.getChildren().addAll(urlBox, hBox, boxBoutonValid, swingNode, hBox1);
+
+        scrollPane = new ScrollPane();
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
+        scrollPane.setContent(root);
+
         root.setSpacing(20);
-        Scene scene = new Scene(root, 450, 250);
+        Scene scene = new Scene(scrollPane, 500, 400);
+        this.scene = scene;
+        this.stage = stage;
         stage.setTitle("Verite Terrains");
         stage.setScene(scene);
-
         stage.show();
-
     }
+
 
     private void showCSVTable(String folder, String url) {
+        savedFileName = url;
         ArrayList<String[]> csvLines = readCSV(folder, url);
-        String[] columnsName = csvLines.get(0);
+        columnNames = csvLines.get(0);
 
-        //  table.getColumns().add(new TableColumn<>("Numero"));
-        for (int i = 0; i < columnsName.length; i++) {
-            TableColumn col = new TableColumn<>(columnsName[i]);
-            int finalI = i;
-            col.setCellValueFactory((Callback<TableColumn.CellDataFeatures<ObservableList<String>, String>,
-                    ObservableValue<String>>) param -> new SimpleStringProperty(param.getValue().get(finalI)));
-            table.getColumns().add(col);
-        }
-
+        contentTable = new String[csvLines.size()-1][];
 
         for (int i = 1; i < csvLines.size(); i++) {
-            //Iterate Row
-            String[] rows = csvLines.get(i);
-            ObservableList<String> row = FXCollections.observableArrayList();
-            for (int j = 0; j < columnsName.length; j++) {
-                //Iterate Column
-                row.add(rows[j]);
-            }
-            dataList.add(row);
+            contentTable[i-1] = csvLines.get(i);
+
         }
 
-
-        table.setItems(dataList);
+        table_ = new JTable(contentTable,columnNames);
+        conteneurTableau = new JPanel();
+        conteneurTableau.setLayout(new BorderLayout());
+        conteneurTableau.add(new JScrollPane(table_), BorderLayout.CENTER);
+        createSwingContent(swingNode);
     }
+
+    private void createSwingContent(SwingNode swingNode){
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                JPanel panel = new JPanel();
+                panel.add(table_);
+                swingNode.setContent(panel);
+            }
+        });
+    }
+
+    private void save(String fileName){
+        System.out.println("file contents are saved in "+fileName);
+
+
+        String outputFileName = "src/verites/"+fileName;
+
+
+        try {
+            String contenu = String.join(",", Arrays.<String>asList(columnNames));
+            for(String[] content: contentTable){
+                contenu += "\r\n"+String.join(",", Arrays.<String>asList(content));
+            }
+            System.out.println("columnsName "+columnNames.length);
+            System.out.println("content "+contentTable.length);
+            System.out.println("DELIMITER "+contenu);
+
+            FileWriter writer = new FileWriter(outputFileName, false);
+            writer.write(contenu);
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 
     private ArrayList<String[]> readCSV(String folder, String csvFileName) {
         ArrayList<String[]> lines = new ArrayList<>();
-        String CsvFile = "src/main/output/" + folder + "/" + csvFileName;
+        String csvFile = "src/main/output/" + folder + "/" + csvFileName;
+        System.out.println(csvFile);
+
         String FieldDelimiter = ",";
 
         BufferedReader br;
         try {
-            br = new BufferedReader(new FileReader(CsvFile));
+            br = new BufferedReader(new FileReader(csvFile));
             String line;
             while ((line = br.readLine()) != null) {
                 String[] fields = line.split(FieldDelimiter, -1);
@@ -205,7 +289,6 @@ public class Verite extends Application {
         } catch (IOException ex) {
             ex.printStackTrace();
         }
-
         return lines;
     }
 }
